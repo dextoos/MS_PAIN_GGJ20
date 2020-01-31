c8348e89a1bb778fad8e5d2a938f657753ef2fb5
NGageMegaman <gerardgery1@gmail.com>
2020-01-31 20:12:56 +0100

    First Push

---
+++ Assets/Scripts/Fondo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fondo : MonoBehaviour
+{
+
+    public float dt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dt = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dt += Time.deltaTime;
+        if (dt > 1.0f/60.0f)
+        {
+            dt -= 1.0f / 60.0f;
+            if (Input.GetKey("right"))
+                this.transform.position = this.transform.position + new Vector3(0.1f, 0.0f, 0.0f);
+            if (Input.GetKey("left"))
+                this.transform.position = this.transform.position + new Vector3(-0.1f, 0.0f, 0.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public GameObject[] emojiList;
+    public int heldEmojiId;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heldEmojiId = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
